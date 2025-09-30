8b52e3d482bdcacf2338ce4f13fe29fc8c75a342
pkv-woodstock <pkvstarscream@gmail.com>
2025-09-30 16:46:57 +0530

    updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig((command, mode) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  console.log("Vite Env Variables:", env.VITE_DEBUG) // Example of using an env variable
+
+  return {
+    plugins: [react()],
+    server: {
+      ...(env.VITE_DEBUG === 'true' && {
+        proxy: {
+          '/api': {
+            target: 'http://localhost:8000',
+            changeOrigin: true,
+            secure: false,
+          }
+        }
       }
+      )
     }
   }
 })
